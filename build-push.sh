e246432106447ce2349ea4452c63630cf868745f
Brian Feister <brian@drm.st>
2017-06-01 19:43:06 -0400

    fix change of ownership problem with `cp`

--- build-push.sh
+++ build-push.sh
@@ -27,7 +27,7 @@ git checkout $target_branch
 echo 'copying static files to project root'
 
 # copy the static site files into the current directory.
-cp -R _book/* .
+scp -rp _book/* .
 
 msg='commiting changes and pushing to '
 echo $msg$target_branch
